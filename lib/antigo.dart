17ff87585b06531ffd3ed1daadefff41a849c3c5
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-12-09 12:24:40 -0300

    Implementando novo Layout

---
+++ lib/antigo.dart
@@ -0,0 +1,130 @@
+//
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//     theme: ThemeData(
+//       primarySwatch: (Colors.deepPurple),
+//     ),
+//     home: Scaffold(
+//         appBar: AppBar(title: const Text('Exemplo de Column')),
+//         body:
+//
+//         Stack(                                                      // Precione Alt + Enter para envolver o Widget com outro.
+//           children: [                                               // Um layout que permite sobrepor widgets uns sobre os outros.
+//             Container(                                              // Um widget flexível que combina layout e estilo. Pode ser usado para definir dimensões, bordas, cores, padding, margens e muito mais.
+//               color: Colors.cyan,
+//             ),
+//             SingleChildScrollView(                                  // Permite que o conteúdo seja rolado se exceder os limites do contêiner.
+//
+//               child: Column(                                                 // Organiza seus filhos (widgets) em uma disposição vertical (de cima para baixo), proporcionando uma maneira simples e eficaz de construir layouts verticais.
+//
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,     // Distribui os containers com espaçamento igual entre eles.
+//                 crossAxisAlignment: CrossAxisAlignment.center,        // Centraliza os containers horizontalmente.
+//
+//                 children: [                                           // Assim como o Stack, possui a propriedade Children que permite agrupar vários Widgets.
+//                   // O primeiro Filho é uma Fileira de Containers e os outros 3 são Containers separados.
+//                   Row(                                                // O Row é um widget do Flutter usado para organizar widgets filhos em uma disposição horizontal (lado a lado). Ele é o equivalente horizontal do Column e é muito útil para criar layouts onde os elementos precisam ser exibidos lado a lado.
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Distribui os containers com espaçamento igual entre eles.
+//                     crossAxisAlignment: CrossAxisAlignment.center,    // Centraliza os containers verticalmente.
+//
+//                     children: [                                        // Assim como o Stack, possui a propriedade Children que permite agrupar vários Widgets.
+//                       Stack(
+//                         alignment: AlignmentDirectional.center,
+//                         children: [
+//                           Container(color: Colors.red, width: 100, height: 100,),
+//                           Container(color: Colors.blue, width: 50, height: 50,
+//                             child: const Center(child: Text('Box 1')),),
+//                         ],
+//                       ),
+//                       Stack(
+//                         alignment: AlignmentDirectional.center,
+//                         children: [
+//                           Container(color: Colors.red, width: 100, height: 100,),
+//                           Container(color: Colors.blue, width: 50, height: 50,
+//                             child: const Center(child: Text('Box 2')),),
+//                         ],
+//                       ),
+//                       Stack(
+//                         alignment: AlignmentDirectional.center,
+//                         children: [
+//                           Container(color: Colors.red, width: 100, height: 100,),
+//                           Container(color: Colors.blue, width: 50, height: 50,
+//                             child: const Center(child: Text('Box 3')),),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//
+//                   Container(
+//                     width: 100,
+//                     height: 300,
+//                     color: Colors.red,
+//                     margin: EdgeInsets.all(10),
+//                     child: const Center(child: Text(
+//                       'Este é um texto muito longo que pode causar overflow.',          // "overflow" ocorre quando um widget tenta renderizar conteúdo que excede os limites do seu contêiner pai.
+//                       overflow: TextOverflow.ellipsis,
+//                     )),
+//                   ),
+//                   Container(
+//                     width: 100,
+//                     height: 100,
+//                     color: Colors.green,
+//                     margin: EdgeInsets.all(10),
+//                     child: const Center(child: Text('Box 2')),
+//                   ),
+//                   Container(
+//                     width: 100,
+//                     height: 100,
+//                     color: Colors.blue,
+//                     margin: EdgeInsets.all(10),
+//                     child: const Center(child: Text('Box 3')),
+//                   ),
+//                   Container(
+//                     height: 50,
+//                     width: 450,
+//                     margin: const EdgeInsets.all(16),                 // Ajuste de Margem. Adiciona 16 de margem em todos os lados.
+//                     padding: const EdgeInsets.only(top: 10),          // Ajuste espaço interno entre o conteúdo e as bordas do widget.
+//                     decoration: BoxDecoration(                        // O DecoratedBox é uma ferramenta poderosa para estilização no Flutter e, em conjunto com outros widgets, ajuda a construir interfaces elegantes e responsivas.
+//                       color: Colors.yellow,
+//                       border: Border.all(color: Colors.black, width: 2),  // Define uma borda preta
+//                       borderRadius: BorderRadius.circular(10),            // Adiciona cantos arredondados
+//                     ),
+//                     child: const Text(                                    // Text é responsável por exibir texto na interface do usuário.
+//                       'Container Amarelo com Box Decoration ',          // String Data
+//                       style: TextStyle(
+//                         color: Colors.grey,
+//                         fontSize: 16,
+//                       ),
+//                       textAlign: TextAlign.center,
+//                     ),
+//                   ),
+//                   ElevatedButton(
+//                       onPressed: (){print("Botão Pressionado");},      // Gatilho de Botão
+//                       child: Text("Elevated Button")),
+//                   ElevatedButton(
+//                     onPressed: () {
+//                       print('Botão estilizado pressionado!');
+//                     },
+//                     style: ElevatedButton.styleFrom(
+//                       elevation: 10, // Elevação
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(12),      // Bordas arredondadas
+//                       ),
+//                       padding: const EdgeInsets.symmetric(
+//                         horizontal: 24,
+//                         vertical: 12,
+//                       ),                                              // Espaçamento interno
+//
+//                     ),
+//                     child: const Text(
+//                       'Elevated Button Estilizado',
+//                       style: TextStyle(fontSize: 16),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         )
+//     ),
+//   );
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -14,127 +14,144 @@ class MyApp extends StatelessWidget {
         primarySwatch: (Colors.deepPurple),
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Exemplo de Column')),
-        body:
+          appBar: AppBar(
+              title: const Text('Tarefas',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+              backgroundColor: Colors.blue,
+          ),
 
-        Stack(                                                      // Precione Alt + Enter para envolver o Widget com outro.
-          children: [                                               // Um layout que permite sobrepor widgets uns sobre os outros.
-            Container(                                              // Um widget flexível que combina layout e estilo. Pode ser usado para definir dimensões, bordas, cores, padding, margens e muito mais.
-              color: Colors.cyan,
-            ),
-            SingleChildScrollView(                                  // Permite que o conteúdo seja rolado se exceder os limites do contêiner.
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                  child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 15),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(
+                                    5), // Define o raio da borda
+                              ),
+                              width: MediaQuery.of(context).size.width,
+                              height: 150,
+                              margin: EdgeInsets.all(10),
+                            ),
+                            Stack(children: [
+                              Container(
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 2,
+                                        blurRadius: 5,
+                                        offset: Offset(0, 3),
+                                      ),
+                                    ],
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(
+                                        5), // Define o raio da borda
+                                  ),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 120,
+                                  margin: EdgeInsets.symmetric(horizontal: 10)),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment:
+                                    CrossAxisAlignment.center,
 
-              child: Column(                                                 // Organiza seus filhos (widgets) em uma disposição vertical (de cima para baixo), proporcionando uma maneira simples e eficaz de construir layouts verticais.
-              
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,     // Distribui os containers com espaçamento igual entre eles.
-                crossAxisAlignment: CrossAxisAlignment.center,        // Centraliza os containers horizontalmente.
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(
+                                          5), // Define o raio da borda
+                                    ),
+                                    width: 100,
+                                    height: 120,
+                                    margin: EdgeInsets.only(left: 10),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        child: Text('Aprender Flutter'),
+                                      ),
+                                      Container(
+                                        child: Text('estrelas'),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    width: 60,
+                                    height: 80,
+                                    margin: EdgeInsets.only(right: 20),
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                          print(
+                                              'Botão estilizado pressionado!');
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.blue,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                            horizontal: 0,
+                                            vertical: 0,
+                                          ),
+                                        ),
+                                        child: const Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.arrow_drop_up,
+                                              color: Colors.white,
+                                            ),
+                                            Text(
+                                              'Lvl Up',
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 10),
+                                            ),
 
-                children: [                                           // Assim como o Stack, possui a propriedade Children que permite agrupar vários Widgets.
-                                                                      // O primeiro Filho é uma Fileira de Containers e os outros 3 são Containers separados.
-                  Row(                                                // O Row é um widget do Flutter usado para organizar widgets filhos em uma disposição horizontal (lado a lado). Ele é o equivalente horizontal do Column e é muito útil para criar layouts onde os elementos precisam ser exibidos lado a lado.
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Distribui os containers com espaçamento igual entre eles.
-                    crossAxisAlignment: CrossAxisAlignment.center,    // Centraliza os containers verticalmente.
-              
-                    children: [                                        // Assim como o Stack, possui a propriedade Children que permite agrupar vários Widgets.
-                      Stack(
-                        alignment: AlignmentDirectional.center,
-                        children: [
-                          Container(color: Colors.red, width: 100, height: 100,),
-                          Container(color: Colors.blue, width: 50, height: 50,
-                            child: const Center(child: Text('Box 1')),),
-                        ],
-                      ),
-                      Stack(
-                        alignment: AlignmentDirectional.center,
-                        children: [
-                          Container(color: Colors.red, width: 100, height: 100,),
-                          Container(color: Colors.blue, width: 50, height: 50,
-                            child: const Center(child: Text('Box 2')),),
-                        ],
-                      ),
-                      Stack(
-                        alignment: AlignmentDirectional.center,
-                        children: [
-                          Container(color: Colors.red, width: 100, height: 100,),
-                          Container(color: Colors.blue, width: 50, height: 50,
-                            child: const Center(child: Text('Box 3')),),
-                        ],
-                      ),
-                    ],
-                  ),
-                  
-                  Container(
-                    width: 100,
-                    height: 300,
-                    color: Colors.red,
-                    margin: EdgeInsets.all(10),
-                    child: const Center(child: Text(
-                      'Este é um texto muito longo que pode causar overflow.',          // "overflow" ocorre quando um widget tenta renderizar conteúdo que excede os limites do seu contêiner pai.
-                      overflow: TextOverflow.ellipsis,
-                    )),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                    margin: EdgeInsets.all(10),
-                    child: const Center(child: Text('Box 2')),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                    margin: EdgeInsets.all(10),
-                    child: const Center(child: Text('Box 3')),
-                  ),
-                  Container(
-                    height: 50,
-                    width: 450,
-                    margin: const EdgeInsets.all(16),                 // Ajuste de Margem. Adiciona 16 de margem em todos os lados.
-                    padding: const EdgeInsets.only(top: 10),          // Ajuste espaço interno entre o conteúdo e as bordas do widget.
-                    decoration: BoxDecoration(                        // O DecoratedBox é uma ferramenta poderosa para estilização no Flutter e, em conjunto com outros widgets, ajuda a construir interfaces elegantes e responsivas.
-                      color: Colors.yellow,
-                      border: Border.all(color: Colors.black, width: 2),  // Define uma borda preta
-                      borderRadius: BorderRadius.circular(10),            // Adiciona cantos arredondados
-                    ),
-                    child: const Text(                                    // Text é responsável por exibir texto na interface do usuário.
-                        'Container Amarelo com Box Decoration ',          // String Data
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                          ),
-                        textAlign: TextAlign.center,
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: (){print("Botão Pressionado");},      // Gatilho de Botão
-                      child: Text("Elevated Button")),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Botão estilizado pressionado!');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      elevation: 10, // Elevação
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),      // Bordas arredondadas
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 12,
-                      ),                                              // Espaçamento interno
-              
-                    ),
-                    child: const Text(
-                      'Elevated Button Estilizado',
-                      style: TextStyle(fontSize: 16),
+                                          ],
+                                        )),
+                                  )
+                                ],
+                              ),
+                            ]),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ],
-              ),
-            ),
-          ],
-        )
-      ),
+              ))
+            ],
+          )),
     );
   }
 }
