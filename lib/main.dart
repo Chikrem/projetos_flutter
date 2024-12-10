17bdbb8f3449f6afbd5f9ddc05658238b4ba69d4
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-12-10 09:36:09 -0300

    Animated Opacity

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: (Colors.deepPurple),
-      ),
-      home: Scaffold(
+        theme: ThemeData(
+          primarySwatch: (Colors.deepPurple),
+        ),
+        home: Scaffold(
           appBar: AppBar(
             title: const Text(
               'Tarefas',
@@ -21,18 +28,48 @@ class MyApp extends StatelessWidget {
             ),
             backgroundColor: Colors.blue,
           ),
-          body: ListView(
-                    children: [
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 3),
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 2),
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 4),
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 1),
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 5),
-                      Card('Flutter 0', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 3),
-                    ],
-              )
-      )
-    );
+          body: AnimatedOpacity(
+            opacity: opacidade ? 1 : 0,
+            duration: Duration(milliseconds: 1000),
+            child: ListView(
+              children:[
+                Padding(
+                  padding: EdgeInsets.only(top:8),
+                  child: Card(
+                      'Estudar Flutter',
+                      'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                      3),
+                ),
+                Card(
+                    'Andar de Bike',
+                    'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                    2),
+                Card(
+                    'Ler 50 páginas',
+                    'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                    1),
+                Card(
+                    'Meditar',
+                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                    4),
+                Card('Jogar',
+                    'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+                     1),
+                const SizedBox(
+                  height: 100,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState((){
+                opacidade = !opacidade;
+              });
+            },
+            child: Icon(Icons.remove_red_eye),
+          ),
+        ));
   }
 }
 
@@ -41,22 +78,18 @@ class Card extends StatefulWidget {
   final String foto;
   final int dif;
 
-  Card (this.nome, this.foto, this.dif, {super.key});
+  Card(this.nome, this.foto, this.dif, {super.key});
 
   @override
   State<Card> createState() => _CardState();
 }
 
 class _CardState extends State<Card> {
-
   int nivel = 0;
   int barra = 0;
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Container(
@@ -82,48 +115,48 @@ class _CardState extends State<Card> {
                   width: MediaQuery.of(context).size.width,
                   height: 160,
                   margin: EdgeInsets.all(10),
-
                   child: Column(
                     children: [
                       Stack(children: [
                         Container(
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 2,
-                                  blurRadius: 5,
-                                  offset: Offset(0, 3),
-                                ),
-                              ],
-                              color: Colors.white,
-                              borderRadius:
-                              BorderRadius.circular(5), // Define o raio da borda
-                            ),
-                            width: MediaQuery.of(context).size.width,
-                            height: 120,
-                            ),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(
+                                5), // Define o raio da borda
+                          ),
+                          width: MediaQuery.of(context).size.width,
+                          height: 120,
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             Container(
                               decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
                                 color: Colors.blueGrey,
-                                borderRadius:
-                                BorderRadius.circular(5), // Define o raio da borda
                               ),
                               width: 100,
                               height: 120,
-                              child: Image.network(
-                                   widget.foto,
-                                   fit: BoxFit.cover),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.network(
+                                  widget.foto,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
-
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
-
                               children: [
                                 Container(
                                   width: 170,
@@ -131,24 +164,42 @@ class _CardState extends State<Card> {
                                     widget.nome,
                                     style: const TextStyle(
                                         fontSize: 24,
-                                        overflow: TextOverflow.ellipsis
-                                    ),
+                                        overflow: TextOverflow.ellipsis),
                                   ),
                                 ),
                                 Container(
                                   child: Row(
                                     children: [
-                                      Icon(Icons.star, size: 15, color: widget.dif >= 1 ? Colors.blue : Colors.blue[100]),
-                                      Icon(Icons.star, size: 15, color: widget.dif >= 2 ? Colors.blue : Colors.blue[100]),
-                                      Icon(Icons.star, size: 15, color: widget.dif >= 3 ? Colors.blue : Colors.blue[100]),
-                                      Icon(Icons.star, size: 15, color: widget.dif >= 4 ? Colors.blue : Colors.blue[100]),
-                                      Icon(Icons.star, size: 15, color: widget.dif >= 5 ? Colors.blue : Colors.blue[100]),
+                                      Icon(Icons.star,
+                                          size: 15,
+                                          color: widget.dif >= 1
+                                              ? Colors.blue
+                                              : Colors.blue[100]),
+                                      Icon(Icons.star,
+                                          size: 15,
+                                          color: widget.dif >= 2
+                                              ? Colors.blue
+                                              : Colors.blue[100]),
+                                      Icon(Icons.star,
+                                          size: 15,
+                                          color: widget.dif >= 3
+                                              ? Colors.blue
+                                              : Colors.blue[100]),
+                                      Icon(Icons.star,
+                                          size: 15,
+                                          color: widget.dif >= 4
+                                              ? Colors.blue
+                                              : Colors.blue[100]),
+                                      Icon(Icons.star,
+                                          size: 15,
+                                          color: widget.dif >= 5
+                                              ? Colors.blue
+                                              : Colors.blue[100]),
                                     ],
                                   ),
                                 ),
                               ],
                             ),
-
                             Container(
                               width: 60,
                               height: 80,
@@ -156,7 +207,6 @@ class _CardState extends State<Card> {
                               child: ElevatedButton(
                                   onPressed: () {
                                     setState(() {
-
                                       if (nivel < 10) {
                                         barra++;
                                         if (barra == 10) {
@@ -179,7 +229,8 @@ class _CardState extends State<Card> {
                                   ),
                                   child: const Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
                                     children: [
                                       Icon(
                                         Icons.arrow_drop_up,
@@ -196,10 +247,8 @@ class _CardState extends State<Card> {
                           ],
                         ),
                       ]),
-
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(8.0),
@@ -207,14 +256,19 @@ class _CardState extends State<Card> {
                                 width: 200,
                                 child: LinearProgressIndicator(
                                   color: Colors.white,
-                                  value: (widget.dif > 0) ? (barra/widget.dif)/10 : 1,
+                                  value: (widget.dif > 0)
+                                      ? (barra / widget.dif) / 10
+                                      : 1,
                                 )),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8),
                             child: Text(
-                              (nivel == 10) ? ('Nível: Max') : ('Nível: $nivel' ),
-                              style: TextStyle(color: Colors.white, fontSize: 16),
+                              (nivel == 10)
+                                  ? ('Nível: Max')
+                                  : ('Nível: $nivel'),
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
                             ),
                           ),
                         ],
@@ -230,4 +284,3 @@ class _CardState extends State<Card> {
     );
   }
 }
-
