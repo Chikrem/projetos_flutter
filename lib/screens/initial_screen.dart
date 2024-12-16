6c918aa7de5f5bf9b450cd0ad3c3241262a56b18
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-12-16 17:53:41 -0300

    Inherited Widgets

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -23,26 +23,7 @@ class _InitialScreenState extends State<InitialScreen> {
       ),
       body: ListView(
         children: const [
-          Task(
-              'Aprender Flutter',
-              'assets/images/dash.png',
-              3),
-          Task(
-              'Andar de Bike',
-              'assets/images/bike.webp',
-              2),
-          Task(
-              'Meditar',
-              'assets/images/meditar.jpeg',
-              5),
-          Task(
-              'Ler',
-              'assets/images/livro.jpg',
-              4),
-          Task('Jogar',
-              'assets/images/jogar.jpg',
-              1
-          ),
+          // Lista de Tarefas
           SizedBox(height: 80,)
         ],
       ),
