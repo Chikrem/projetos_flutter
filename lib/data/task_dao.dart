8d50481a0fc09637688744bbb185e56cd0ea7b41
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-12-18 17:17:59 -0300

    Criando findAll() e find() para CRUD com .query sqflite

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:projetos_flutter/components/task.dart';
+import 'package:projetos_flutter/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
 
@@ -14,8 +16,45 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async {}
-  Future<List<Task>> find(String nomeDaTarefa) async {}
+
   delete(String nomeDaTarefa) async {}
+
+  List<Task> toList(List<Map<String,dynamic>> mapaDeTarefas){
+
+    print('Convertendo to List');
+
+    final List<Task> tarefas = [];
+
+    for (Map<String,dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    };
+
+    print('Lista de Tarefas: $tarefas');
+
+    return tarefas;
+  }
+
+  // query is for reading a table content. It returns a list of map.
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll: ');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(_tablename);
+    print('Procurando dados no banco de dados... encontrado: $result');
+    return toList(result);
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {
+    print('Acessando find: ');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result = await bancoDeDados.query(
+        _tablename,
+        where: '$_name = ?',
+        whereArgs: [nomeDaTarefa]
+    );
+    print('Tarefa encontrada: ${toList(result)}');
+    return toList(result);
+  }
+  
 
 }
